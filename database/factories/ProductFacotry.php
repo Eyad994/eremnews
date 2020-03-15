0bfeac3e7f821a428ce898bf1e0117c203614590
Eyad <46415579+Eyad994@users.noreply.github.com>
2020-03-15 09:42:13 +0200

    Update ProductFacotry.php

--- database/factories/ProductFacotry.php
+++ database/factories/ProductFacotry.php
@@ -10,6 +10,7 @@ $factory->define(Product::class, function (Faker $faker) {
         'product_name' => $faker->word,
         'product_price' => $faker->numberBetween(1, 30),
         'product_desc' => $faker->text,
+        'product_image' => $faker->randomElement($array = array ('1','2','3', '4', '5', '6', '7', '8', '9', '10')).'.jpg',
         'category_id' => $faker->numberBetween(1, 5),
         'product_image' => 'photo1.jpg',
         'user_id' => $faker->numberBetween(1, 5),
